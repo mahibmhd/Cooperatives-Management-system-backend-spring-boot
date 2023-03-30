933f31bb0e754a217a182b5fec189b2862e536f7
coop.ihub@gmail.com <coop.ihub@gmail.com>
2023-03-30 12:10:15 +0300

    members model modifed

--- src/main/java/com/dxvalley/project/models/Member.java
+++ src/main/java/com/dxvalley/project/models/Member.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -29,5 +32,15 @@ public class Member {
     private String dateCreated;
     @OneToOne(cascade = CascadeType.ALL)
     private Address address;
+
+    //asset with union
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "union_Id")
+	private Unions unionId;
+
+    //asset with prCooperative
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "prCooperative_Id")
+	private PrCooperative prCooperativeId;
     
 }
